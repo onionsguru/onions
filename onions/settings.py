2290153958c7c7107c75c335de4fcced4110244e
onionsguru <lee@onns.co.kr>
2019-02-23 08:00:56 +0900

    aws01

--- onions/settings.py
+++ onions/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = '3sta@9a1r)64)1fe=kzuz6^8(0@%fvyky*%ehzp6y#-9fr*&a4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['django-env.z7ydqw27gd.ap-northeast-2.elasticbeanstalk.com', 'localhost', '127.0.0.1']
-
+ALLOWED_HOSTS = ['env01.t5dpmmmpm9.ap-northeast-2.elasticbeanstalk.com', 'localhost', '127.0.0.1']
 
 # Application definition
 
