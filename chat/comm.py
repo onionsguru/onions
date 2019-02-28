6d03d5e1912a7f087e320a351a6945d73c807002
onionsguru <lee@onns.co.kr>
2019-02-28 10:38:05 +0900

    minor

--- chat/comm.py
+++ chat/comm.py
@@ -99,7 +99,7 @@ class ChatChannel(AsyncWebsocketConsumer):
         for u in chatroom.user_set.all():
             if u.auth_name != '':         
                 if u.is_owner:
-                    chatter_list.append('<b>' + u.nickname_text+ ' (' + u.auth_name + ')</b>&#9819;<br>')
+                    chatter_list.append('<b>' + u.nickname_text+ ' (' + u.auth_name + ')</b> &#9819;<br>')
                 else:
                     chatter_list.append(u.nickname_text+' (' + u.auth_name + ')')
             else:
